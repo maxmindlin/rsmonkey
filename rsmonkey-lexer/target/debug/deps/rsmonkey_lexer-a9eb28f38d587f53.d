35b726433b9a2178053cb7a1d1a586c0d182dd81
Max Mindlin <maxmindlin@gmail.com>
2021-03-07 16:47:17 -0500

    rm target dir

--- rsmonkey-lexer/target/debug/deps/rsmonkey_lexer-a9eb28f38d587f53.d
+++
@@ -1,8 +0,0 @@
-/Users/maxmindlin/dev/rsmonkey/rsmonkey-lexer/target/debug/deps/rsmonkey_lexer-a9eb28f38d587f53.rmeta: src/lib.rs src/token.rs
-
-/Users/maxmindlin/dev/rsmonkey/rsmonkey-lexer/target/debug/deps/librsmonkey_lexer-a9eb28f38d587f53.rlib: src/lib.rs src/token.rs
-
-/Users/maxmindlin/dev/rsmonkey/rsmonkey-lexer/target/debug/deps/rsmonkey_lexer-a9eb28f38d587f53.d: src/lib.rs src/token.rs
-
-src/lib.rs:
-src/token.rs:
--- rsmonkey-lexer/target/debug/deps/rsmonkey_lexer-c869a01b7fc68c53.d
+++
@@ -1,6 +0,0 @@
-/Users/maxmindlin/dev/rsmonkey/rsmonkey-lexer/target/debug/deps/rsmonkey_lexer-c869a01b7fc68c53.rmeta: src/lib.rs src/token.rs
-
-/Users/maxmindlin/dev/rsmonkey/rsmonkey-lexer/target/debug/deps/rsmonkey_lexer-c869a01b7fc68c53.d: src/lib.rs src/token.rs
-
-src/lib.rs:
-src/token.rs:
